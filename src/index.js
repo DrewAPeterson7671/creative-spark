c7a1e68cdc202d45099f656596ce5ded11cd94fa
Student <example@example.com>
2020-03-08 14:26:22 -0700

    Fix index.js crash by changing index .js to document.getElementById(root) from react-app-root.  Im not sure what the consequences of that will be.  But it displays now

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './components/App';
-import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import reducer from './reducers/reducer'
+import reducer from './reducers/reducer';
 import { HashRouter } from 'react-router-dom';
 
 const store = createStore(reducer);
@@ -17,22 +15,8 @@ ReactDOM.render(
       <Component/>
     </Provider>
   </HashRouter>,
-  document.getElementById('react-app-root')
+  document.getElementById('root')
 );
 };
 
 render(App);
-
-/*eslint-disable */
-if (module.hot) {
-  module.hot.accept('./components/App', () => {
-    render(App);
-  });
-}
-/*eslint-enable */
-
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
